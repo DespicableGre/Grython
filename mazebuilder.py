a0f2e9d82078aea9208eabf7bd05d94324e2e918
Gre <98617173+DespicableGre@users.noreply.github.com>
2024-10-16 19:09:09 -0500

    Updated mazebuilder.py

--- mazebuilder.py
+++ mazebuilder.py
@@ -1,55 +1,42 @@
+# LEBRON OF CODING
 import turtle as trtl
 
-empty = "empty.gif"
-wall = "wall.gif"
-filled = "filled.gif"
-other = "other.gif"
-
+shapes = ["empty.gif", "wall.gif", "filled.gif", "other.gif"]
 wn = trtl.Screen()
+wn.addshape(shapes[0])
+wn.addshape(shapes[1])
+wn.addshape(shapes[2])
+wn.addshape(shapes[3])
 
-wn.addshape(empty)
-wn.addshape(wall)
-wn.addshape(filled)
-wn.addshape(other)
-
-builder = trtl.Turtle(shape=empty)
+builder = trtl.Turtle(shapes[0])
 #painter = trtl.Turtle()
 
 builder.penup()
-builder.speed(1)
+builder.speed(0)
 
 # Start building
 
 maze = [ 16 , 16 ]
+startingpos = [ -(maze[0]-1)/2*16 , (maze[1]-1)/2*16 ]
 
-startingpos = [ 0 , 0 ]
-
-if maze[0] % 2 != 0:
-    startingpos[0] = (maze[0] - 1) / 2
-else:
-    startingpos[0] = (maze[0] / 2) - .5
-if maze[1] % 2 != 0:
-    startingpos[1] = (maze[1] - 1) / 2
-else:
-    startingpos[1] = (maze[1] / 2) - .5
-
-startingpos = [ -startingpos[0] * 16, startingpos[1] * 16 ]
-builder.goto(startingpos)
+# Make grid
 for y in range(maze[1]):
-    builder.setheading(0)
-    for x in range(maze[0]-1):
+    builder.goto(startingpos[0],startingpos[1]-16*y)
+    builder.stamp()
+    for x in range(maze[0] - 1):
+        builder.fd(16)
         builder.stamp()
-        builder.forward(16)
-        builder.stamp()
-    builder.goto(startingpos[0], (y - .5) * 16)
+
+# Generate Maze
 
 
 
-painter = trtl.Turtle(shape=filled)
-painter2 = trtl.Turtle(shape=wall)
-painter3 = trtl.Turtle(shape=wall)
-painter2.goto(16,16)
-painter3.goto(-16,16)
+# forgot to mention that this was to know if it was centered
+painter = trtl.Turtle(shapes[2])
+# painter2 = trtl.Turtle(shape=wall)
+# painter3 = trtl.Turtle(shape=wall)
+# painter2.goto(16,16)
+# painter3.goto(-16,16)
 
 
 trtl.mainloop()
